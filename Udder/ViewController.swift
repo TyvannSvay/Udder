8e3b93d84d24ead8561eb340124b7bf58342b6c6
Tyvann Svay <tyvann.svay@Tyvanns-MacBook-Pro.local>
2019-01-16 14:54:23 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tyvann Svay on 16/01/19.
+//  Copyright © 2019 Tyvann Svay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
